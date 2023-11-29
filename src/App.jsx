e056ef282b23e9f417fce5a9f2ea35e9b5f5ecc9
f-iturburu <f.iturburu@outlook.com>
2023-11-29 16:40:54 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import "./App.css";
+import { Container, Form, Button } from "react-bootstrap";
+import { ColorCard } from "./components/ColorCard";
+import { useState } from "react";
+
+function generateHexColor() {
+  let str = "0123456789abcdef";
+  let color = "";
+  for (let i = 0; i < 6; i++) {
+    let index = Math.floor(Math.random() * str.length);
+    color += str[index];
+  }
+  color = "#" + color;
+  return color;
+}
+
+function App() {
+  return (
+    <Container fluid className="text-center">
+      <Form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <div className="d-flex my-3 justify-content-center">
+          <Form.Control
+            className="w-50"
+            min="1"
+            max="30"
+            type="number"
+            placeholder="Inserte la cantidad de colores que desea generar (entre 1 y 30)"
+          />
+          <Button type="submit" className="ms-3" variant="primary">
+            Generar
+          </Button>
+        </div>
+      </Form>
+    </Container>
+  );
+}
+
+export default App;
